a27933ca23dfba3fcae88fd28cb93832d428b02f
padmini1797 <pmaduri380@gmail.com>
2023-05-28 23:31:57 +0530

    qsall

--- .vscode/qs.js
+++ .vscode/qs.js
@@ -1,4 +1,10 @@
-var secItem=document.querySelector('.list-group-item:nth-child(2)');
+/*var secItem=document.querySelector('.list-group-item:nth-child(2)');
 secItem.style.backgroundColor='green';
 var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.display='none'
+thirdItem.style.display='none'*/
+var sec=document.querySelectorAll('li');
+sec[1].style.backgroundColor='lightgreen';
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+}
