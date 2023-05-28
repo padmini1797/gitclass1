16a668f0c60d3b00fbe8c473275c3f6b062c64d9
padmini1797 <pmaduri380@gmail.com>
2023-05-28 23:17:56 +0530

    queryselector

---
+++ .vscode/qs.js
@@ -0,0 +1,4 @@
+var secItem=document.querySelector('.list-group-item:nth-child(2)');
+secItem.style.backgroundColor='green';
+var thirdItem=document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display='none'
