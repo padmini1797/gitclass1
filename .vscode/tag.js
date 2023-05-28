267ee65a0b619154ee578f551ee31905e88c0cd3
padmini1797 <pmaduri380@gmail.com>
2023-05-28 22:43:16 +0530

    gettagname

--- .vscode/tag.js
+++ .vscode/tag.js
@@ -1,6 +1,13 @@
-var items=document.getElementsByClassName("list-group-item");
+/*var items=document.getElementsByClassName("list-group-item");
 items[2].textContent='hello';
 items[1].style.backgroundColor='pink';
 for(i=0;i<items.length;i++){
     items[i].style.fontWeight='bold'
+}*/
+var list=document.getElementsByTagName("li");
+list[2].textContent='hello';
+list[1].style.backgroundColor='pink';
+for(i=0;i<list.length;i++){
+    list[i].style.fontWeight='bold'
 }
+
