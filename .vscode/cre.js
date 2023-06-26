3f8914236aeee23f5aa72fedf0c2e349625dfdba
padmini1797 <pmaduri380@gmail.com>
2023-06-26 23:03:48 +0530

    scaling upfor manyusers

--- .vscode/cre.js
+++ .vscode/cre.js
@@ -1,6 +1,7 @@
 const myForm = document.getElementById('my-form');
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
+const mobInput = document.getElementById('phone');
 myForm.addEventListener('submit', onSubmit);
 function onSubmit(e) {
     e.preventDefault();
@@ -10,11 +11,19 @@ function onSubmit(e) {
         setTimeout(() => msg.remove(), 3000);
     }
     else{
-        let obj={
+        var uk=document.createElement('LI');
+        newdiv.appendChild(uk);
+        var obj={
             namei:nameInput.value,
-            emaili:emailInput.value
-        }
-        localStorage.setItem('details',JSON.stringify(obj));
-
-        }
+            emaili:emailInput.value,
+            phonei:mobInput.value
+        };
+        var t=document.createTextNode(`${obj.namei}-${obj.emaili}-${obj.phonei}`);
+        uk.append(t);
+        localStorage.setItem(obj.emaili,JSON.stringify(obj));
+        
     }
+    
+    
+
+}
